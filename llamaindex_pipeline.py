5ebeb5d714ddb5e3b69f87aec00816cc9038ab1d
Kiran-Vijapure <142876353+Kiran-Vijapure@users.noreply.github.com>
2025-08-12 20:38:12 +0530

    Update llamaindex_pipeline.py

--- llamaindex_pipeline.py
+++ llamaindex_pipeline.py
@@ -36,7 +36,7 @@ class Pipeline:
             **{
                 "llm_end_point": os.getenv("LLM_END_POINT", "dummy-llm-end-point"),
                 "llm_api_key": os.getenv("LLM_API_KEY", "dummy-llm-api-key"),
-                "openai_apikey": os.getenv("APIKEY", "dummy-openai-key"),
+                "openai_apikey": os.getenv("APIKEY", "dummy-openai-key")
             }
         )
 
